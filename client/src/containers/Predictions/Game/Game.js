e9dc9930faaa5c0858595c840b8c12802ac30058
Tommats <tommats5@gmail.com>
2018-03-03 11:10:05 +0200

    Basic game edition first settings

--- client/src/containers/Predictions/Game/Game.js
+++ client/src/containers/Predictions/Game/Game.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Game.css';
 
+class Game extends Component {
+  state = {
+    editable: true,
+    homePrediction: 0,
+    awayPrediction: 0
+  }
+  componentWillMount () {
+    this.setState({homePrediction: this.props.predictedHome, awayPrediction: this.props.predictedAway});
+  }
+  render() {
+    return (
+      <div className={classes.Game}>
+        {(this.props.status === 'FINISHED') ? <span className={classes.Closed}>Closed</span>  : null }
+        <p>{this.props.date}</p>
+        <p>{this.props.home} vs {this.props.away} {(this.props.status === 'FINISHED') ? <span className={classes.FinalResult}>{this.props.homeGoals}-{this.props.awayGoals}</span>  : null }</p>
+        <p>My prediction: <span className={classes.PredictionBox}>{this.state.homePrediction}</span> - <span className={classes.PredictionBox}>{this.state.awayPrediction}</span></p>
+      </div>
+    );
+  }
 
-const game = (props) => (
-  <div className={classes.Game}>
-    {(props.status === 'FINISHED') ? <span className={classes.Closed}>Closed</span>  : null }
-    <p>{props.date}</p>
-    <p>{props.home} vs {props.away} {(props.status === 'FINISHED') ? <span className={classes.FinalResult}>{props.homeGoals}-{props.awayGoals}</span>  : null }</p>
-    <p>My prediction: {props.predictedHome} - {props.predictedAway}</p>
-  </div>
-);
+}
 
-export default game;
+export default Game;
